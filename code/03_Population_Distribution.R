0149818bdbb889a3b5535a4348a768d54893594e
LeaGRichards <lea.giguere.richards@gmail.com>
2024-10-16 15:45:41 +0200

    Update 03_Population_Distribution.R

--- code/03_Population_Distribution.R
+++ code/03_Population_Distribution.R
@@ -1,3 +1,11 @@
 ### Population Distribution ###
 
-# Install Packages needed
+# Install Packages needed and open them with library function
+  # sdm = species distribution modelling
+  # terra = spatial data analysis
+
+install.packages("terra")
+install.packages("sdm")
+
+library(terra)
+library(sdm)
