5aa8d95c43abe4fd75baf6fed5be4e0b0bb07efb
LeaGRichards <lea.giguere.richards@gmail.com>
2024-10-17 15:51:44 +0200

    Update 03_Population_Distribution.R

--- code/03_Population_Distribution.R
+++ code/03_Population_Distribution.R
@@ -27,8 +27,33 @@ plot(rana)
   pres <- rana[rana$Occurrence==1]
   pres
   pres$Occurrence
-
-  # Make multiframe made of complete rana dataset and pres dataset
-  par(mfrow=c(1,2))
+  
+  # Make dataset of only occurance = 0 (absence of rana) by using sequel (sql) language
+    # Absence is more uncertain as a dataset than the presence 
+    # as when present = sure to have right data, absence, could be missing a present rana)
+  abse <- rana[rana$Occurrence==0]
+  abse
+  
+  # Make multiframe made of complete rana dataset and abse/pres dataset
+  par(mfrow=c(2,2))
+  plot(pres, main="Presence Only")
+  plot(abse, main="Absence Only")
   plot(rana, main="Complete")
-  plot(pres, main="Presence only")
+  
+  # Plot presences in blue and absences in red in the same graph
+  dev.off()
+  plot(pres, col="blue", pch=19, cex=2)
+  points(abse, col="red", pch=19, cex=2)
+
+# Covariates : try to explain rana distribution with elevation data
+  # All .asc files are raster datasets  -- :) cool -- 
+  # Using the rast function to change data into a raster dataset
+  elev <- system.file("external/elevation.asc", package="sdm")
+  elevmap <- rast(elev)
+  
+  # Make plot with elev and rana datasets divided in pres/abse with funky colours
+  cln<-colorRampPalette(c("firebrick","darkseagreen","darkslateblue"))(100)
+  plot(elevmap, col=cln )
+  points(pres, col="yellow", pch=19, cex=1)
+  points(abse, col="darkorchid4", pch=19, cex=1)
+  
