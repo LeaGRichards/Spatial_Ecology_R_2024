6cf8456f82052c0809021dce4f03ede6fdb56685
LeaGRichards <lea.giguere.richards@gmail.com>
2024-10-17 15:06:22 +0200

    Update 03_Population_Distribution.R

--- code/03_Population_Distribution.R
+++ code/03_Population_Distribution.R
@@ -9,3 +9,26 @@ install.packages("sdm")
 
 library(terra)
 library(sdm)
+
+# Bring up dataset to work on from the sdm package by using system.file function
+  # necessary to call package="" as the folder external is in all packages
+file <- system.file("external/species.shp", package="sdm")
+file
+
+# Translate .shp file to a usable file in R (SpatVector) with vect function 
+rana <- vect(file)
+rana$Occurrence
+
+# Plot Distribution of rana 
+plot(rana)
+
+  # Make dataset of only occurance = 1 (presence of rana) by using sequel (sql) language
+    # all statements in sql function end with ; rahter than :
+  pres <- rana[rana$Occurrence==1]
+  pres
+  pres$Occurrence
+
+  # Make multiframe made of complete rana dataset and pres dataset
+  par(mfrow=c(1,2))
+  plot(rana, main="Complete")
+  plot(pres, main="Presence only")
